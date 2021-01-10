a21cf31a42f54e916fb46c9a39ffcbd0edbfdb53
Sandeep Singh <sandeepgaurdec13@gmail.com>
2021-01-11 00:02:12 +0530

    Added:Create Contact in Address Book

--- AddressBook.java
+++ AddressBook.java
@@ -1,10 +1,56 @@
 /**
  * @author Sandeep_Singh
- * @Functionality Welcome to AddressBook Problem. 
+ * @Functionality Create Contact in Address Book. 
  *
  */
 public class AddressBook {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book !");
-    }
+	private String city;
+	private String state;
+	private String zipcode;
+	private String email;
+
+	public AddressBook(String city, String state, String zipcode, String email) {
+	this.city=city;
+	this.state=state;
+	this.email=email;
+	this.zipcode=zipcode;
+
+	}
+
+	public String toString() {
+		return " [city=" + city + ", state=" + state + ", zipcode=" + zipcode + ", email=" + email ;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getemail() {
+		return email;
+	}
+
+	public void setemail(String email) {
+		this.email = email;
+	}
+
 }
